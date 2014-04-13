57d33e192d9322b3aaf5612106acd4805def1847
Clay <c.ewing@miami.edu>
2014-04-12 22:41:25 -0400

    initial

---
+++ ClimateTrip/src/thought.cpp
@@ -0,0 +1,9 @@
+//
+//  thought.cpp
+//  ClimateTrip
+//
+//  Created by Clay Ewing on 4/12/14.
+//
+//
+
+#include "thought.h"
---
+++ ClimateTrip/src/thought.h
@@ -0,0 +1,14 @@
+//
+//  thought.h
+//  ClimateTrip
+//
+//  Created by Clay Ewing on 4/12/14.
+//
+//
+
+#ifndef __ClimateTrip__thought__
+#define __ClimateTrip__thought__
+
+#include <iostream>
+
+#endif /* defined(__ClimateTrip__thought__) */
